3b637590eb425b6c32465ec0ca6adb42f8c797ac
Emilly Cristina <emilly.c.f@hotmail.com>
2024-01-18 21:19:42 -0300

    updated database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  /* username: process.env.DB_USERNAME,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT, */ 
-  url: process.env.DB_URL,
+  port: process.env.DB_PORT,
+  //url: process.env.DB_URL,
   dialect: 'postgres',
   define: {
     timestamps: true,
