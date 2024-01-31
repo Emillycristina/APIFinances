fa81aa545804e49006e966ccbda37723e68d25d0
Emilly Cristina <emilly.c.f@hotmail.com>
2024-01-31 18:25:51 -0300

    update

--- src/server.js
+++ src/server.js
@@ -4,15 +4,16 @@ const PORT = process.env.PORT || 3030;
 const cors = require('cors');
 const passport = require('../app/Controllers/Passport');
 
-app.use(passport.initialize());
-
 app.use(cors({
   origin: '*',  
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //credentials: true,
+  credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204
 }));
+
+app.use(passport.initialize());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
