7720e0dfc6dd16f221ba417f02a830cc3a495941
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-01 21:46:23 -0300

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,8 @@
 const addCustomHeaders = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Origin: http://localhost:3000');
+    res.header('Access-Control-Allow-Methods : GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Custom-Header', 'Valor do Cabeçalho Personalizado');
-    // Adicione outros cabeçalhos necessários aqui
+   
     next();
   };
 
