ecc7e9aad19dcd9993cba7e9f499d8a23fbb7190
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-11 17:30:55 -0300

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 const addCustomHeaders = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin: http://localhost:3000');
-    res.header('Access-Control-Allow-Methods : GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
     res.header('Access-Control-Allow-Credentials', true);
    
     next();
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   preflightContinue: false,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 200
 }));
 
 
