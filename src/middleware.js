723a7b6c588e9808bccee3b174a3f739f73bd01b
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-11 19:42:10 -0300

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 const addCustomHeaders = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://finances-front-gilt.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://finances-front-gilt.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type','Authorization')
     res.header('Access-Control-Allow-Credentials', true);
