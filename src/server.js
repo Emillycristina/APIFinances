4f28f0cbf689ebf61f2df594490ddefe07e9d0b7
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-12 20:43:13 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 
 const app = require('./app');
 const PORT = process.env.PORT || 3030;
-//const cors = require('cors');
-const { addCustomHeaders } = require('./middleware');
+const cors = require('cors');
 const passport = require('../app/Controllers/Passport');
 
 
+const addCustomHeaders = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://finances-front-gilt.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type','Authorization')
+  res.header('Access-Control-Allow-Credentials', true);
+ 
+  app.use(cors());
+  
+  next();
+};
 
 app.use(addCustomHeaders);
 
