f1b9d3b15d008a43ad665fc9ab249a720c7846ef
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-14 16:45:09 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -2,15 +2,26 @@ const express = require('express');
 const passport = require('../app/Controllers/Passport'); // Substitua pelo caminho real do seu arquivo Passport.js
 const routes = require('./routes');
 const convertDateMiddleware = require('../app/Controllers/ConverterDate');
+const cors = require('cors');
 
 require('./database');
 
 require('dotenv').config();
 
+const corsOptions = {
+  origin: 'https://finances-front-gilt.vercel.app',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204 
+}
+
+
 class App {
   constructor() {
     this.app = express();
 
+    this.app.use(cors(corsOptions))
     this.middlewares();
     this.routes();
   }
--- src/server.js
+++ src/server.js
@@ -1,24 +1,8 @@
 
 const app = require('./app');
 const PORT = process.env.PORT || 3030;
-const cors = require('cors');
-const passport = require('../app/Controllers/Passport');
 
 
-const corsOptions = {
-  origin: ['https://finances-front-gilt.vercel.app',' https://finances-front-gilt.vercel.app/Login'],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 204 
-}
-
-app.use(cors(corsOptions));
-
-
-app.use(passport.initialize());
-
-app.options('*', cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
